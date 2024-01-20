380360ed3ffe8bacc5b410b106d365c86872fe8a
Giulio de Cadilhac <g.decadilhac@gmail.com>
2024-01-20 01:38:41 +0100

    mark guard location

--- scripts/executeTransaction.ts
+++ scripts/executeTransaction.ts
@@ -1,15 +1,15 @@
-import SafeApiKit from '@safe-global/api-kit';
-import { EthersAdapter } from '@safe-global/protocol-kit';
-import { MetaTransactionData } from '@safe-global/safe-core-sdk-types';
-import dotenv from 'dotenv';
-import { ethers } from 'ethers';
-import Safe from '@safe-global/protocol-kit';
+import SafeApiKit from "@safe-global/api-kit";
+import { EthersAdapter } from "@safe-global/protocol-kit";
+import { MetaTransactionData } from "@safe-global/safe-core-sdk-types";
+import dotenv from "dotenv";
+import { ethers } from "ethers";
+import Safe from "@safe-global/protocol-kit";
 
 dotenv.config();
 
 const RPC_URL = process.env.RPC_ENDPOINT_URL;
 const provider = new ethers.JsonRpcProvider(RPC_URL);
-const SAFE_ADDRESS = '0x9b7a9C49280a6AEAB7b9375ac0Cb5BEFd861F75B';
+const SAFE_ADDRESS = "0x9b7a9C49280a6AEAB7b9375ac0Cb5BEFd861F75B";
 const ownerSigner = new ethers.Wallet(process.env.PRIVATE_KEY!, provider);
 
 const ethAdapter = new EthersAdapter({
@@ -35,18 +35,21 @@ async function executeTranaction() {
   const safeTxHash = transaction.safeTxHash;
 
   const safeTransaction = await safeService.getTransaction(safeTxHash);
+
+  // Reentrancy guard
+
   const executeTxResponse = await safeSdkOwner.executeTransaction(
     safeTransaction
   );
   const receipt = await executeTxResponse.transactionResponse?.wait();
 
-  console.log('Transaction executed:');
+  console.log("Transaction executed:");
 
   const afterBalance = await safeSdkOwner.getBalance();
   console.log(
     `The final balance of the Safe: ${ethers.formatUnits(
       afterBalance,
-      'ether'
+      "ether"
     )} ETH`
   );
 }
